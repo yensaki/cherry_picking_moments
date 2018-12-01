375d440bbcdb2909e0a165367c3e409b9efb318e
yensaki <mov.an.double@gmail.com>
2018-12-01 23:02:56 +0900

    Impl cherry-picking

--- main.rb
+++ main.rb
@@ -12,14 +12,14 @@ phash_map = CSV.parse(output).each.with_object({}) do |row, map|
   next unless File.exist?(filepath)
 
   if map[phash]
+    puts "delete! duplicated: #{phash}: #{filename}"
     FileUtils.rm(filepath)
   else
     map[phash] = filename
   end
-end
+end.invert
 
-phashes = phash_map.keys
-simillaries = {}
+filenames = phash_map.keys.sort
 duplicateds = []
 
 def to_binary(phash)
@@ -30,36 +30,27 @@ def hamming_distance(source, compared)
   source.split('').zip(compared.split('')).count { |i, j| i.to_i ^ j.to_i == 1 }
 end
 
-phashes.each do |phash_a|
+filenames.each do |filename_a|
+  filepath_a = File.join(target_path, filename_a)
+  next unless File.exist?(filepath_a)
+  phash_a = phash_map[filename_a]
   # next if duplicateds.include?(phash_a)
 
-  phashes.each do |phash_b|
-    # next if duplicateds.include?(phash_b)
-    next if phash_a == phash_b
+  filenames.each do |filename_b|
+    filepath_b = File.join(target_path, filename_b)
+    next unless File.exist?(filepath_b)
 
+    phash_b = phash_map[filename_b]
+    # next if duplicateds.include?(phash_b)
     diff =  hamming_distance(to_binary(phash_a), to_binary(phash_b))
-    puts "#{diff}, file: #{phash_map[phash_a]}, #{phash_map[phash_b]}"
-    if diff <= 20
-      simillaries[phash_a] ||= []
-      simillaries[phash_a] |= [phash_a, phash_b]
-      duplicateds += [phash_a, phash_b]
+    puts "#{diff}, file: #{filename_a}: #{phash_a}, #{filename_b}: #{phash_b}"
+    if diff <= 10
+      puts "delete! #{filename_b}"
+      FileUtils.rm(filepath_b)
+      duplicateds << phash_b
     end
   end
+  filenames.delete(filename_a)
 end
 
 puts "dupulicated #{duplicateds.size}"
-
-count = 0
-simillaries.each_value do |array|
-  array.delete(array.sample)
-  array.each do |phash|
-    filename = phash_map[phash]
-    puts filename
-
-    filepath = File.join(target_path, filename).to_s
-    FileUtils.rm(filepath) if File.exist?(filepath)
-    count += 1
-  end
-end
-
-puts "deleted #{count}"
