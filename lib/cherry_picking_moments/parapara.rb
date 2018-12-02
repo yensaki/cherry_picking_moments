2bdb85aefda1011ffe945df066534b3e3f41296e
yensaki <mov.an.double@gmail.com>
2018-12-02 16:35:07 +0900

    Set inner CherryPickingMoments Module

--- lib/cherry_picking_moments/parapara.rb
+++ lib/cherry_picking_moments/parapara.rb
@@ -1,25 +1,27 @@
 require "open3"
 require "fileutils"
 
-class Parapara
-  def initialize(file, output_path, rate: 3)
-    @file = file
-    @output_path = output_path
-    @rate = rate
-  end
-
-  def slice!
-    tmpdir = File.join("/tmp", "parapara_#{Time.now.to_i}")
-    FileUtils.mkdir_p(tmpdir)
-    cmd = "ffmpeg -ss 0 -i #{@file} -vsync 2 -r #{@rate} -an -f image2 '#{File.join(tmpdir, "%04d.png")}'"
-    _output, _error, _status = Open3.capture3(cmd)
-
-    Dir.glob(File.join(tmpdir, "*.png")) do |filepath|
-      output_base_name = File.basename(filepath).gsub("png", "jpg")
-      outout_file_path = File.join(@output_path, output_base_name)
-      Open3.capture3("convert #{filepath} -resize 960x540 #{outout_file_path}")
-      File.delete(filepath)
+module CherryPickingMoments
+  class Parapara
+    def initialize(file, output_path, rate: 3)
+      @file = file
+      @output_path = output_path
+      @rate = rate
     end
-    Dir.rmdir(tmpdir)
+
+    def slice!
+      tmpdir = File.join("/tmp", "parapara_#{Time.now.to_i}")
+      FileUtils.mkdir_p(tmpdir)
+      cmd = "ffmpeg -ss 0 -i #{@file} -vsync 2 -r #{@rate} -an -f image2 '#{File.join(tmpdir, "%04d.png")}'"
+      _output, _error, _status = Open3.capture3(cmd)
+
+      Dir.glob(File.join(tmpdir, "*.png")) do |filepath|
+        output_base_name = File.basename(filepath).gsub("png", "jpg")
+        outout_file_path = File.join(@output_path, output_base_name)
+        Open3.capture3("convert #{filepath} -resize 960x540 #{outout_file_path}")
+        File.delete(filepath)
+      end
+      Dir.rmdir(tmpdir)
+    end
   end
 end
