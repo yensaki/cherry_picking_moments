b49d278ae64f8635238c076630e4fe81ab602abe
yensaki <mov.an.double@gmail.com>
2019-01-22 23:00:06 +0900

    Get clean .uniquish!

--- lib/cherry_picking_moments.rb
+++ lib/cherry_picking_moments.rb
@@ -13,20 +13,7 @@ module CherryPickingMoments
     phash_map = ImageHash.phash_map(target_path)
     psimilar_images = SimilarImages.new(phash_map, target_path)
     psimilar_images.uniquish!(threshold: 20)
-
-    dhash_map = ImageHash.dhash_map(target_path)
-    dsimilar_images = SimilarImages.new(dhash_map, target_path)
-    dsimilar_images.uniquish!(threshold: 20)
-
-    ahash_map = ImageHash.ahash_map(target_path)
-    asimilar_images = SimilarImages.new(ahash_map, target_path)
-    asimilar_images.uniquish!(threshold: 20)
-
     puts "phash deleted duplicated: #{psimilar_images.duplicateds.size}"
     puts "phash deleted neary: #{psimilar_images.deleted_nearies.size}"
-    puts "dhash deleted duplicated: #{dsimilar_images.duplicateds.size}"
-    puts "dhash deleted neary: #{asimilar_images.deleted_nearies.size}"
-    puts "ahash deleted duplicated: #{asimilar_images.duplicateds.size}"
-    puts "ahash deleted neary: #{asimilar_images.deleted_nearies.size}"
   end
 end
