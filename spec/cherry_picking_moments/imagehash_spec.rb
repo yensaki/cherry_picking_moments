50dc0d150f374ae9b43f688a71cd3c09c7a3bca4
yensaki <mov.an.double@gmail.com>
2019-01-26 00:59:30 +0900

    Add Test about SimilarImages

--- spec/cherry_picking_moments/imagehash_spec.rb
+++ spec/cherry_picking_moments/imagehash_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe CherryPickingMoments::ImageHash do
     subject { CherryPickingMoments::ImageHash.phash_map(target_path) }
     let(:target_path) { spec_dir.join("fixtures/images") }
     it 'gets phash' do
-      is_expected.to eq({ 'bd86c2393dc6c239' => ['image001.png'] })
+      is_expected.to eq({ "bd86c2393dc6c239"=>["image001.png"], "d52928d6efa89427"=>["image002.png"] })
     end
   end
 
@@ -20,7 +20,7 @@ RSpec.describe CherryPickingMoments::ImageHash do
     subject { CherryPickingMoments::ImageHash.dhash_map(target_path) }
     let(:target_path) { spec_dir.join("fixtures/images") }
     it 'gets phash' do
-      is_expected.to eq({ '0000401616680000' => ['image001.png'] })
+      is_expected.to eq({"0000401616680000"=>["image001.png"], "a449b44a4ab4b640"=>["image002.png"]})
     end
   end
 
@@ -28,7 +28,7 @@ RSpec.describe CherryPickingMoments::ImageHash do
     subject { CherryPickingMoments::ImageHash.ahash_map(target_path) }
     let(:target_path) { spec_dir.join("fixtures/images") }
     it 'gets phash' do
-      is_expected.to eq({ 'ffffff8383ffffff' => ['image001.png'] })
+      is_expected.to eq({"0000defbaefe0000"=>["image002.png"], "ffffff8383ffffff"=>["image001.png"]})
     end
   end
 end
