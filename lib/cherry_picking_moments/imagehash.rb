725a1b636602f5ed12f52f767d4fce7899d8b123
yensaki <mov.an.double@gmail.com>
2019-06-29 15:27:14 +0900

    Objective Phash

--- lib/cherry_picking_moments/imagehash.rb
+++ lib/cherry_picking_moments/imagehash.rb
@@ -9,20 +9,27 @@ module CherryPickingMoments
     def self.phash_map(target_path)
       phash_map = []
       Dir.glob(File.join(target_path, '*.png')) do |filepath|
-        pyimage = Image.open(filepath)
-        phash = imagehash.phash(pyimage).to_s
-        phash_map << Phash.new(filepath, phash)
+        phash_map << Phash.new(filepath)
       end
       phash_map
     end
 
     class Phash
-      attr_reader :filepath, :phash
+      attr_reader :filepath
       attr_accessor :next_image_diff
 
-      def initialize(filepath, phash)
+      def initialize(filepath)
         @filepath = filepath
-        @phash = phash
+      end
+
+      def phash
+        @phash ||= imagehash.phash(pyimage).to_s
+      end
+
+      private
+
+      def pyimage
+        @pyimage ||= Image.open(@filepath)
       end
     end
   end
